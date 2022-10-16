3bc174d8686703d87a51d6da0c8df8305e0c7ac5
LaDunne15 <vladilsav7772@gmail.com>
2022-10-16 16:01:18 +0300

    Heroku config5

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  });
 const port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(port,() => {
+    console.log(`Production Express server running at localhost:${port}`);
+  });
